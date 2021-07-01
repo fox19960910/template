eab5ec273eff0f94fb15450c4b480969577781da
Le Xuan Tung <tunglx@hybrid-technologies.vn>
2021-07-01 16:38:15 +0700

    update mobile menu

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-1. Please make sure you already have node
+1. Please make sure you already have node,gulp ( npm i gulp -g ), sass (npm i node-sass)
 2. install package: npm install
 3. use SASS: gulp
--- src/js/components.js
+++ src/js/components.js
@@ -18,10 +18,19 @@ let components = {
     // hambuger button
     const hambugerBtn = document.querySelector("#hambuger");
     const navMenu = document.querySelector("#navAreaMenu");
+    const navArea = document.querySelector("#navArea");
+    const overlay = document.createElement('div')
+    overlay.classList.add('overlayBig')
+    
     hambugerBtn.addEventListener("click", () => {
-      console.log("click");
-      //   hambugerBtn.classList.toggle("active");
-      navMenu.classList.toggle("active");
+      console.log("click",overlay);
+        hambugerBtn.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        if(document.querySelector('.overlayBig')){
+          overlay.remove()
+        }else{
+          navArea.appendChild(overlay)
+        }
     });
   },
 };
